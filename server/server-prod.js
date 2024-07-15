b781e1357cb2ac76d6899471b6ba51ca2e60a098
Jim Talbut <jim.talbut@groupgti.com>
2024-07-15 18:10:50 +0100

    Segregate server into separate project

--- server/server-prod.js
+++ server/server-prod.js
@@ -3,19 +3,19 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
-import apiRouter from './server/apiRouter.js';
+import apiRouter from './apiRouter.js';
  
 const app = express();
  
 app.use(apiRouter)
-app.use(express.static(path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'dist/client'), { index: false }));
+app.use(express.static(path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../dist/client'), { index: false }));
  
 app.use('*', async (_, res) => {
   try {
-    const template = fs.readFileSync('./dist/client/index.html', 'utf-8');
+    const template = fs.readFileSync('../dist/client/index.html', 'utf-8');
     res.status(200).set({ 'Content-Type': 'text/html' }).end(template);
   } catch (error) {
-    res.status(500).end(error);
+    res.status(500).end(error.toString());
   }
 });
  
