5dc80abf342de227fb51c72ea9f5ac1ec3b2c6b2
Jim Talbut <jim.talbut@groupgti.com>
2024-08-11 08:51:13 +0100

    Add details dialog for checks

--- src/ChecksUi.tsx
+++ src/ChecksUi.tsx
@@ -4,6 +4,11 @@ import Section from "./Section";
 import { Check, CheckArguments } from "./checks/checks";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import Box from "@mui/system/Box";
+import Link from "@mui/material/Link";
 
 interface ChecksUiProps {
   id: string
@@ -23,21 +28,74 @@ class CheckResultsCategory {
     this.data.push(row)
   }
 }
-function evaluate(check: Check, args: CheckArguments) {
-  try {
-    const result = check.evaluate(args)
-    return [check.id
-      , {children: (<Tooltip placement="bottom-start" title={check.description}><Typography>{check.title}</Typography></Tooltip>), value: check.title}
-      , result.state, result.threshold, result.value, result.message]
-  } catch (ex) {
-    return [check.id
-      , {children: (<Tooltip placement="bottom-start" title={check.description}><span>{check.title}</span></Tooltip>), value: check.title}
-      , 'error', null, null, String(ex)]
-  }
+
+interface CheckDialogProps {
+  check?: Check
+  , onClose: () => void
 }
+function CheckDialog(props: CheckDialogProps) {
+
+  const open: boolean = props.check ? true : false
+  return (
+    <Dialog open={open} onClose={() => props.onClose()}>
+      <DialogTitle sx={{fontWeight: 700}}>
+        {props.check?.category} {props.check?.id} {props.check?.title}
+      </DialogTitle>
+      <Box sx={{ padding: '0px 24px 32px 24px' }}>
+      {props.check?.description &&
+          (
+            <Box>
+              <Typography>{props.check.description}</Typography>
+            </Box>
+          )
+        }
+        {props.check?.remediation &&
+          (
+            <Box>
+              <h4>Remediation</h4>
+              <Typography>{props.check.remediation}</Typography>
+            </Box>
+          )
+        }
+        {props.check?.remediationImpact &&
+          (
+            <Box>
+              <h4>Remediation Impact</h4>
+              <Typography>{props.check.remediationImpact}</Typography>
+            </Box>
+          )
+        }
+        {props.check?.reference &&
+          (
+            <Box>
+              <h4>Reference</h4>
+              <Link href={props.check.reference} target="_blank" rel="noreferrer"><Typography>{props.check.reference}</Typography></Link>
+            </Box>
+          )
+        }
+      </Box>
+    </Dialog>
+  );
+}
+
 function ChecksUi(props: ChecksUiProps) {
 
+  const [checkDetails, setCheckDetails] = useState<Check | undefined>()
+
   const headers = ['ID', 'CHECK', 'RESULT', 'THRESHOLD', 'VALUE', 'MESSAGE']
+
+  function evaluate(check: Check, args: CheckArguments) {
+    try {
+      const result = check.evaluate(args)
+      return [check.id
+        , { children: (<Tooltip placement="bottom-start" title={check.description}><Typography onClick={() => setCheckDetails(check)}>{check.title}</Typography></Tooltip>), value: check.title }
+        , result.state, result.threshold, result.value, result.message]
+    } catch (ex) {
+      return [check.id
+        , { children: (<Tooltip placement="bottom-start" title={check.description}><span>{check.title}</span></Tooltip>), value: check.title }
+        , 'error', null, null, String(ex)]
+    }
+  }
 
   const data = props.checks.reduce((acc, check) => {
     const row = evaluate(check, props.args)
@@ -53,19 +111,22 @@ function ChecksUi(props: ChecksUiProps) {
     <Grid container spacing={2} >
       {data.map(cat => {
         return (
-          <Section id={props.id} heading={cat.category} xs={12}>
-            <DataTable id={props.id + ".table"} headers={headers} rows={cat.data} rowStyle={r => {
-              switch (r[2]) {
-                case 'fail':
-                  return { background: 'red' }
-                case 'error':
-                  return { background: 'darkred' }
-                case 'warning':
-                  return { background: 'yellow' }
-              }
-            }} >
-            </DataTable>
-          </Section>
+          <>
+            <Section id={props.id} heading={cat.category} xs={12}>
+              <DataTable id={props.id + ".table"} headers={headers} rows={cat.data} rowStyle={r => {
+                switch (r[2]) {
+                  case 'fail':
+                    return { background: 'red' }
+                  case 'error':
+                    return { background: 'darkred' }
+                  case 'warning':
+                    return { background: 'yellow' }
+                }
+              }} >
+              </DataTable>
+            </Section>
+            <CheckDialog check={checkDetails} onClose={() => setCheckDetails(undefined)} />
+          </>
         )
       })}
     </Grid>
