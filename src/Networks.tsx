b7a4f4fe56223b41a430e42887c3c3efabfb26e8
Jim Talbut <jim.talbut@groupgti.com>
2024-09-24 01:09:32 +0100

    Networks using MaterialTable

--- src/Networks.tsx
+++ src/Networks.tsx
@@ -1,10 +1,47 @@
 import { useState, useEffect } from 'react';
-import DataTable, { DataTablePropsEntry } from './DataTable';
 import { Network } from './docker-schema'
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import { DockerApi } from './DockerApi';
+import { MRT_ColumnDef } from 'material-react-table';
+import { Link } from 'react-router-dom';
+import MaterialTable from './MaterialTable';
+
+interface NetworkDetails {
+  id: string
+  name?: string
+  driver?: string
+  scope?: string
+  encrypted?: string
+}
+const networkColumns: MRT_ColumnDef<NetworkDetails>[] = [
+  {
+    accessorKey: 'id',
+    header: 'ID',
+    size: 220,
+    Cell: ({ renderedCellValue, row }) => (<Link to={"/network/" + row.original.id} >{renderedCellValue}</Link>)
+  },
+  {
+    accessorKey: 'name',
+    header: 'NAME',
+    size: 400,
+  },
+  {
+    accessorKey: 'driver',
+    header: 'DRIVER',
+    size: 150,
+  },
+  {
+    accessorKey: 'scope',
+    header: 'SCOPE',
+    size: 160,
+  },
+  {
+    accessorKey: 'encrypted',
+    header: 'ENCRYPTED',
+    size: 180,
+  },
+]
+
+const heightOffset = 128
 
 interface NetworksProps {
   baseUrl: string
@@ -14,43 +51,53 @@ interface NetworksProps {
 }
 function Networks(props: NetworksProps) {
 
-  const [data, setData] = useState<(string | undefined | DataTablePropsEntry)[][]>()
+  const [networks, setNetworks] = useState<NetworkDetails[]>([])
+  const [maxHeight, setMaxHeight] = useState<Number>(window.innerHeight - heightOffset)
+  const [maxWidth, setMaxWidth] = useState<Number>(window.innerWidth)
 
   useEffect(() => {
+    const handleResize = () => {
+      setMaxHeight(window.innerHeight - heightOffset);
+      setMaxWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+        window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    props.setTitle('Networks')
     props.docker.networks()
       .then(j => {
-        props.setTitle('Networks')
-        const newData = [] as (string | undefined | DataTablePropsEntry)[][]
-        j.forEach((net: Network) => {
-          newData.push(
-            [
-              net.Id ? { link: '/network/' + net.Id, value: net.Id } : ''
-              , net.Name
-              , net.Driver
-              , net.Scope
-              , net?.Options?.encrypted
-            ]
-          )
-        });
-        setData(newData)
+        setNetworks(
+          j.reduce((result: NetworkDetails[], net: Network) => {
+            if (net.Id) {
+              result.push(
+                {
+                  id: net.Id
+                  , name: net.Name
+                  , driver: net.Driver
+                  , scope: net.Scope
+                  , encrypted: net?.Options?.encrypted
+                }
+              )
+            }
+            return result
+          }, [] as NetworkDetails[])
+        )
       })
-  }
-    , [props.refresh])
+  }, [props])
 
-  return (<>
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container >
-        <Paper>
-          <DataTable id="networks" headers={
-            ['ID', 'NAME', 'DRIVER', 'SCOPE', 'ENCRYPTED']
-          } rows={data}>
-          </DataTable>
-        </Paper>
-      </Grid>
-    </Box>
-  </>)
-
-
+  return (
+    <MaterialTable id="networks"
+      columns={networkColumns}
+      data={networks}
+      virtual={true}
+      muiTableContainerProps={{ sx: { maxHeight: maxHeight + 'px', maxWidth: maxWidth + 'px' } }}
+    />
+  )
 }
 
 export default Networks;
